694c5030613667c22484215e60f77664aa4775c7
nareh <53708656+nareha@users.noreply.github.com>
2021-01-31 23:13:52 -0800

    Finalize source code

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,38 @@
 import './App.css';
 import Post from './components/Post';
 
+/* images for posts */
+import image from './images/event.png';
+import hacksprint from './images/hacksprint.png';
+import impact from './images/impact.png';
+
+const posts = [
+  {
+    account: 'ACM at UCLA',
+    image: image
+  },
+  {
+    account: 'Eric Yang',
+    image: hacksprint
+  },
+  {
+    account: 'Nareh Agazaryan',
+    image: impact
+  }
+];
+
 function App() {
   return (
     <div className="App">
-      <Post />
+      {
+        posts.map( post => {
+          return <Post 
+            account={post.account}
+            image={post.image}
+            key={post.account}
+          />
+        })
+      }
     </div>);
   
 }
--- react-app/src/components/Post.js
+++ react-app/src/components/Post.js
@@ -1,17 +1,16 @@
-import image from '../images/event.png';
 import reaction from '../images/reaction.png';
 import like from '../images/like.png';
 import comment from '../images/comment.png';
 import share from '../images/share.png';
 import './Post.css';
 
-function Post() {
+function Post(props) {
     return (
       <div className="postBox">
         <div className="account">
-          <strong>ACM at UCLA</strong>
+          <strong>{props.account}</strong>
         </div>
-        <img className="postImage" src={image}/>
+        <img className="postImage" src={props.image}/>
         <div className="reaction-line">
           <img className="reaction" src={reaction}/>
           <p>Eric, Nareh, and 10 others</p>
